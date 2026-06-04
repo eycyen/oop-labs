4f5e815283fb724f6074d8e19bae9dadb4cac4ed
Ahmet Hasan Canbolat <ahmthsnpriv@gmail.com>
2026-06-04 15:28:45 +0300

    feat: implement Student concrete class and InvalidGPAException exception class

---
+++ lab8/Student.java
@@ -0,0 +1,49 @@
+public class Student extends Person implements Comparable<Student> {
+    private String department;
+    private double gpa;
+
+    public Student(int id, String name, String department, double gpa) throws InvalidGPAException {
+        super(id,name);
+        this.department = department;
+        if (gpa < 0.0 || gpa > 4.0) {
+            throw new InvalidGPAException("GPA must be in between 0.0 and 4.0.");
+        } 
+        this.gpa = gpa;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setGpa(double gpa) throws InvalidGPAException {
+        if (gpa < 0.0 || gpa > 4.0) {
+            throw new InvalidGPAException("GPA must be in between 0.0 and 4.0.");
+        } 
+        this.gpa = gpa;
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public double getGpa() {
+        return this.gpa;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.printf("Student: %d | %s | %s | GPA: %.2f",
+        this.getId(),
+        this.getName(),
+        this.getDepartment(),
+        this.getGpa()
+        );
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        Double currentGpa = Double.valueOf(this.gpa);
+        Double otherGpa = Double.valueOf(other.gpa);
+        return currentGpa.compareTo(otherGpa);
+    }
+}
